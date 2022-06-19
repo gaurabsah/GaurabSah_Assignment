640a8076df0113593e2fd6619ca4429d9bedb686
Gaurab Sah <76423882+gaurabsah@users.noreply.github.com>
2022-06-19 14:01:04 +0530

    added q1 solution

--- Core Java/OOPS/Singleton.java
+++ Core Java/OOPS/Singleton.java
@@ -2,7 +2,7 @@
 //1. static object int the class to be made singleton
 //2. A private constructor inside the class.
 //3. A public static method that returns the static object created int the first step.
-public class singleton {
+public class Singleton {
     public static void main(String[] args) {
         ex object=ex.return_obj();
     }
