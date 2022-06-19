f4f41d94282dcfd0672599e1c7f02ab0a25826db
Gaurab Sah <76423882+gaurabsah@users.noreply.github.com>
2022-06-19 17:00:10 +0530

    Update Ctrl.java

--- Core Java/OOPS/Ctrl.java
+++ Core Java/OOPS/Ctrl.java
@@ -1,21 +1,21 @@
-abstract class persistence{
+abstract class Persistence{
     abstract void persist();
 }
 
-class filePersistence extends persistence{
+class FilePersistence extends Persistence{
     void persist() {
         System.out.println("file persists");
     }
 }
 
-class databasePersistence extends persistence{
+class DatabasePersistence extends Persistence{
     void persist(){
         System.out.println("database persists");
     }
 }
-public class ctrl {
+public class Ctrl {
     public static void main(String[] args) {
-        persistence obj= new persistence() {
+        Persistence obj= new Persistence() {
             @Override
             void persist() {
                 System.out.println("calling it");
