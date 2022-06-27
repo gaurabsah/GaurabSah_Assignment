c8a40f021c6cc7d12be05a4d83565c8b4a60c839
Gaurab Sah <76423882+gaurabsah@users.noreply.github.com>
2022-06-27 09:15:38 +0530

    added StringAssignment.java (Q1 & Q2)

--- Core Java/String/StringAssignment.java
+++ Core Java/String/StringAssignment.java
@@ -1,1 +1,24 @@
+package com.assignment.string;
 
+public class StringAssignment {
+	// Q1
+	static int lengthOfString() {
+		String str = "Hello World";
+		return str.length();
+	}
+
+	// Q2
+	static String mergeOfStrings() {
+		String str1 = "Hello,";
+		String str2 = "How are you?";
+		return str1.concat(str2);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(lengthOfString());
+		System.out.println(mergeOfStrings());
+
+	}
+
+}
