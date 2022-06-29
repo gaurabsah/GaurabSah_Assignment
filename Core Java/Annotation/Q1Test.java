c97333ecf61cac6dda7ef063d95a2eba190b7a78
Gaurab Sah <76423882+gaurabsah@users.noreply.github.com>
2022-06-29 15:49:45 +0530

    added Q1Test.java

---
+++ Core Java/Annotation/Q1Test.java
@@ -0,0 +1,22 @@
+package com.assignment.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//Indicates that the annotated method is a test method.
+//This annotation should be used only on parameterless static methods.
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Test {
+	int testCaseNo();
+}
+
+public class Q1Test {
+	@Test(testCaseNo = 1)
+	public static void main(String[] args) {
+
+	}
+
+}
